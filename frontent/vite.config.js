8c7b7125262bee49714987ce30bb93684bfab29d
Prince Raj <princerajmne@gmail.com>
2026-08-29 21:02:33 +0530

    calling 1

--- frontent/vite.config.js
+++ frontent/vite.config.js
@@ -6,27 +6,23 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'react-vendor';
-            }
-            if (id.includes('recharts')) {
+            if (id.includes('node_modules/recharts')) {
               return 'recharts';
             }
-            if (id.includes('firebase')) {
+            if (id.includes('node_modules/firebase')) {
               return 'firebase';
             }
-            if (id.includes('react-icons') || id.includes('lucide-react')) {
+            if (id.includes('node_modules/jspdf')) {
+              return 'jspdf';
+            }
+            if (id.includes('node_modules/react-icons') || id.includes('node_modules/lucide-react')) {
               return 'icons';
             }
-            if (id.includes('jspdf') || id.includes('axios') || id.includes('socket.io-client')) {
-              return 'vendor-utils';
-            }
-            return 'vendor';
           }
         }
       }
